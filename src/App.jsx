69943687631c0708e50b248d6108ba3c88a61bdb
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-07-24 19:18:57 +0530

    nav created

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
 import './App.css';
 
+import axios from 'axios';
+import { useEffect } from 'react';
+
 function App() {
+  useEffect(() => {
+    axios
+      .get(
+        'http://api.giphy.com/v1/gifs/trending?api_key=cccCgrhtpNLJUQnNJ86pbguPLzWjscn7'
+      )
+      .then((res) => {
+        console.log(res.data);
+      });
+  });
+
   return <></>;
 }
 
---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { SiGiphy } from 'react-icons/si';
+
+const Nav = () => {
+  return <div>Nav</div>;
+};
+
+export default Nav;
