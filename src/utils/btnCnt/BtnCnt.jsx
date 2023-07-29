451fb50f727710e081a6e659a2e715187cbd67e4
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-07-30 00:05:28 +0530

    style added

--- src/utils/btnCnt/BtnCnt.jsx
+++ src/utils/btnCnt/BtnCnt.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { TiArrowBackOutline } from 'react-icons/ti';
 
+import { useNavigate } from 'react-router-dom';
+
 import './btnStyle.css';
 
 const BtnCnt = () => {
+  const navigate = useNavigate();
+
+  const homeBackBtn = () => {
+    navigate('/');
+  };
+
   return (
     <div className='reactBtn-cnt'>
-      <button className='reactBtn'>
+      <button onClick={homeBackBtn} className='reactBtn'>
         <TiArrowBackOutline />
       </button>
     </div>
