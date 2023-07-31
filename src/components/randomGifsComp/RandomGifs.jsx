c8f296d08966479a78c80f7439fc959278cb04c3
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-07-31 14:43:12 +0530

    style added

--- src/components/randomGifsComp/RandomGifs.jsx
+++ src/components/randomGifsComp/RandomGifs.jsx
@@ -1,20 +1,29 @@
 import React, { useContext } from 'react';
 import { apiContext } from '../../dataProvider/DataProvider';
 
+import './randomGifStyle.css';
+
 const RandomGifs = () => {
   const { randomData } = useContext(apiContext);
 
   return (
-    <div>
-      {randomData
-        ? randomData?.map((item) => {
-            return (
-              <div key={item.id}>
-                <img src={item.images.fixed_height.url} alt='' />
-              </div>
-            );
-          })
-        : null}
+    <div className='mainRandom-cnt'>
+      {' '}
+      <div className='randomGif-cnt'>
+        {randomData
+          ? randomData?.map((item) => {
+              return (
+                <div className='randomGif-imgCnt' key={item.id}>
+                  <img
+                    className='randomGifImgs'
+                    src={item.images.fixed_height.url}
+                    alt=''
+                  />
+                </div>
+              );
+            })
+          : null}
+      </div>
     </div>
   );
 };
