22e7316babc3f2ef0efa264bb27f3baead0b6f95
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-08-02 00:33:12 +0530

    style added

--- src/pages/detailsPage/searchDtls/SearchDtlPage.jsx
+++ src/pages/detailsPage/searchDtls/SearchDtlPage.jsx
@@ -4,12 +4,14 @@ import { apiContext } from '../../../dataProvider/DataProvider';
 import BtnCnt from '../../../utils/btnCnt/BtnCnt';
 import DtlsCard from '../../../components/cardComp/DtlsCard';
 
+import './searchDtlStyle.css';
+
 const SearchDtlPage = () => {
   const { searchedData } = useContext(apiContext);
   return (
-    <div>
+    <div className='searchDtl-cnt'>
       <BtnCnt />
-      <div>
+      <div className='searchDtl-card-cnt'>
         {searchedData
           ? searchedData?.map((item) => {
               return (
