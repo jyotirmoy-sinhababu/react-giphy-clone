52df2a453ed0324180a0daeadc708c32fdec461a
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-08-06 12:10:07 +0530

    style added

--- src/pages/detailsPage/navRouteDtls/NavRouteDetailsPage.jsx
+++ src/pages/detailsPage/navRouteDtls/NavRouteDetailsPage.jsx
@@ -2,22 +2,28 @@ import React, { useContext } from 'react';
 import { apiContext } from '../../../dataProvider/DataProvider';
 
 import DtlsCard from '../../../components/cardComp/DtlsCard';
+import BtnCnt from '../../../utils/btnCnt/BtnCnt';
+import './navRouteStyle.css';
 
 const NavRouteDetailsPage = () => {
   const { navData } = useContext(apiContext);
   return (
-    <div className='searchDtl-cnt'>
-      {navData ? (
-        navData.map((item) => {
-          return (
-            <div className='searchDtl-card-cnt' key={item.id}>
-              <DtlsCard item={item} />
-            </div>
-          );
-        })
-      ) : (
-        <div></div>
-      )}
+    <div className='navRoute-cnt'>
+      <BtnCnt />
+      <div className='navRouteSub-cnt'>
+        {' '}
+        {navData ? (
+          navData.map((item) => {
+            return (
+              <div className='navRoute-card-cnt' key={item.id}>
+                <DtlsCard item={item} />
+              </div>
+            );
+          })
+        ) : (
+          <div></div>
+        )}
+      </div>
     </div>
   );
 };
